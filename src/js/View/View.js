0310bbc76b231d4a7f9a13881867c48a6c2b304f
Dejan2504 <krajisnikdejan@gmail.com>
2022-08-13 20:40:42 +0200

    Storing values in UI

--- src/js/View/View.js
+++
@@ -1,3 +0,0 @@
-import * as App from '../controller';
-
-console.log(new App);
--- src/js/controller.js
+++ src/js/controller.js
@@ -1,28 +1,74 @@
 import { API_KEY } from "./config";
 
-class App{
+const clock = document.querySelector('.clock');
+const temperature = document.querySelector('.card h1');
+const humidity = document.querySelector('.humidity');
+const feelLike = document.querySelector('.feellike');
+const pressure = document.querySelector('.pressure');
+const description = document.querySelector('.card-description');
+const city = document.querySelector('.card h2');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
 
-      constructor(){
-            this.setLocation();
-      }
+console.log(input, button);
 
-setLocation(){
-   navigator.geolocation.getCurrentPosition(this.getLocation);
- }
+const getPosition = function () {
+      return new Promise(function (resolve, reject) {
+        navigator.geolocation.getCurrentPosition(
+          position => resolve(position),
+          err => reject(err)
+        );
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+    };
 
-getLocation = async function(position){
-      const {latitude, longitude} = position.coords;   
+const getCounty = async function(){
+      const pos = await getPosition();
+      const {latitude: lat , longitude: lng} = pos.coords;
+      console.log(lat, lng);
 
+      const countryFetch = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+      const countryData = await countryFetch.json();
+
+      city.innerHTML = `Weather in ${countryData.city}`;
+};
+
+const getWeather = async function(){
+      const pos = await getPosition();
+      const {latitude , longitude} = pos.coords;
+      
       const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${latitude},${longitude}?key=${API_KEY}`);
 
       const data = await response.json();
-
+      temperature.innerHTML = data.currentConditions.temp + "℉";
+      humidity.innerHTML = `Humidity: ${data.currentConditions.humidity}%`;
+      feelLike.innerHTML = `Feels like: ${data.currentConditions.feelslike}℉`;
+      pressure.innerHTML = `Pressure: ${data.currentConditions.pressure} mbar`;
+      description.innerHTML = data.description;
      console.log(data);
+};
+
+const renderClock = function(){
+      const date = new Date();
+
+      clock.innerHTML = date.toLocaleTimeString();
+};
+
+const searchCity = function(){
+      const city = input.value;
+      console.log(city);
 }
 
-}
+button.addEventListener('click', searchCity);
 
-export default new App;
+const init = function(){
+      getCounty();
+      renderClock();
+      getWeather();
+};
+
+init();
+
 
 
 
